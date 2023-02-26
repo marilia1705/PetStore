373d5b34a911b8f95ab5a908a2ea7e4099b3f749
Marília Miranda QA <88950899+marilia1705@users.noreply.github.com>
2023-02-26 20:06:50 -0300

    commit inicial para desctop

--- src/main/java/br/com/mariliaqa/Main.java
+++ src/main/java/br/com/mariliaqa/Main.java
@@ -1,4 +1,6 @@
-package br.com.mariliaqa;public class Main {
+package br.com.mariliaqa;
+
+public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
