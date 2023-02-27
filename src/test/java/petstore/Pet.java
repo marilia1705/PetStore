07f12edbefc396564fa807207896bbaf24881f3f
Marília Miranda QA <88950899+marilia1705@users.noreply.github.com>
2023-02-26 22:00:10 -0300

    finalizando post
    incluir Pet

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -1,4 +1,43 @@
+// 1 - pacote
 package petstore;
 
+// 2 -biblioteca
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+
+// 3 - Classes
 public class Pet {
+    //3.1 - Atributos
+    String uri = "https://petstore.swagger.io/v2/pet"; //endereço da entidade Pet
+
+    //3.2 - Metodos e Funções
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String (Files.readAllBytes(Paths.get(caminhoJson)));
+    }
+    //Incluir - Create - Post
+    @Test //Identifica o método ou função como um teste para o TestNG
+    public void incluirPet() throws IOException {
+        String jsonBody = lerJson(caminhoJson:"db/pet1.json");
+
+        //Sintaxe GherKin
+        //Dado - quando - Então
+        //Given - When - Then
+
+        given()
+                .contentType("application/json")
+                .log().all()
+                .body(jsonBody)
+        .when()
+                .post(uri)
+        .then()
+                .log().all()
+                .statusCode(200)
+        ;
+    }
+
 }
